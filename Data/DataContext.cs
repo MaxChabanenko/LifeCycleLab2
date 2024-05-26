736b12e6651fad7b2ae1fd8d0d386f63a2f3cf3c
luca <lucakad@knu.ua>
2024-05-26 14:37:41 +0300

    data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using LifeCycleLab.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LifeCycleLab.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Magazine> Magazines { get; set; }
+    }
+}
